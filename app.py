3e6d370d79b88c5ed5209d24692363eb2d397950
al-gent <94gent@gmail.com>
2025-04-13 23:37:01 -0700

    bugfix

--- app.py
+++ app.py
@@ -213,5 +213,5 @@ def update_map(year_range, n, sort_by, show_all, color, modality):
 
 # Run the app
 if __name__ == '__main__':
-    app.run_server(debug=False, host='0.0.0.0', port=8050)
+    app.run(debug=False, host='0.0.0.0', port=8050)
 
